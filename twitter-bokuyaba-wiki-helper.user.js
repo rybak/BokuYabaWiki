1ec85bf10e727e28ca3b939dd7bfe0e1401e3aa9
Andrei Rybak <rybak.a.v@gmail.com>
2025-06-17 02:02:40 +0200

    twitter-bokuyaba-wiki-helper: format params for short tweets in one line
    When the text of the tweet is short, it is more convenient to have
    wikitext of all parameters on one line.  Format tweets shorter than 20
    (for now) this way.

--- twitter-bokuyaba-wiki-helper.user.js
+++ twitter-bokuyaba-wiki-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter: BokuYaba wiki helper
 // @namespace    https://andrybak.dev
-// @version      7
+// @version      8
 // @description  Helps with adding Twitter citations on BokuYaba wiki
 // @author       Andrei Rybak
 // @license      MIT
@@ -123,6 +123,11 @@
 	}
 
 	function formatCiteTweet(user, number, title, translation) {
+		if (title.length < 20) {
+			return `{{Cite tweet
+|user=${user} |number=${number} |title=${title} |translation=${translation}
+}}`;
+		}
 		return `{{Cite tweet |user=${user} |number=${number}
 |title=${title}
 |translation=${translation}
